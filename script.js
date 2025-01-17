c04a15de547010a8966ecfeb337b4c29d4a51930
aby mane <aby.mane.pro@gmail.com>
2025-01-17 16:12:33 +0100

    cours git

---
+++ script.js
@@ -0,0 +1,34 @@
+// Exercice numero 1
+
+let prenom = "Aby";
+let nom = "MANE";
+let age = 21;
+
+let message = `Je m'appelle ${prenom} ${nom} et j'ai ${age} ans.`;
+alert(message);
+
+
+// Exercice numero 2
+
+let a = 5;
+let b = 3;
+console.log ("Somme :", a + b);
+console.log ("Différence :", a - b);
+console.log ("Produit :", a * b);
+console.log ("Quotient :", a / b);
+
+
+// Exercice numero 3
+
+let metres = 10;
+let centimetres = metres * 100;
+let kilometres = metres / 1000;
+
+
+// Exercice numero 4
+
+let score = 0
+score += 10;       
+score *= 2;           
+score -= 5;
+console.log("Le score final :", score);
